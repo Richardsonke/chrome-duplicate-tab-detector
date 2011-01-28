ec2a5e4a55c041c779ec86493d386535bb41acc7
Kevin Colyar <kevin@colyar.net>
2011-01-28 09:05:54 -0800

    Strips url arguments from url so sites like Gmail work correctly

--- chrome-duplicate-tab-detector.js
+++ chrome-duplicate-tab-detector.js
@@ -1,8 +1,10 @@
 chrome.tabs.onCreated.addListener(function(newTab) {
     chrome.tabs.getAllInWindow(newTab.windowId, function(tabs) {
+        var regex = /\?.*$/;
+        newTab.url = newTab.url.replace(regex, "")
         var duplicateTab = null;
         tabs.forEach(function(otherTab) {
-            if (otherTab.id !== newTab.id && otherTab.url === newTab.url) {
+            if (otherTab.id !== newTab.id && otherTab.url.replace(regex, "") === newTab.url) {
                 duplicateTab = otherTab;
             }
         });
